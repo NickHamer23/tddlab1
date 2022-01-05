14fb884c67f727075d3e92786404863778ac832b
Nick <nhamer233@gmail.com>
2022-01-05 17:53:14 -0500

    finished lab

--- tddlab1.js
+++ tddlab1.js
@@ -1,5 +1,45 @@
 function translate(string) {
-  return null;
+  string = string.toLowerCase();
+  string = string.split(" ");
+  let translated = "";
+
+  for (let i = 0; i < string.length; i++) {
+    let newString = string[i];
+    if (
+      newString.charAt(0) !== "a" &&
+      newString.charAt(0) !== "e" &&
+      newString.charAt(0) !== "i" &&
+      newString.charAt(0) !== "o" &&
+      newString.charAt(0) !== "u" &&
+      newString.charAt(0) !== "y"
+    ) {
+      if (
+        newString.charAt(1) !== "a" &&
+        newString.charAt(1) !== "e" &&
+        newString.charAt(1) !== "i" &&
+        newString.charAt(1) !== "o" &&
+        newString.charAt(1) !== "u" &&
+        newString.charAt(1) !== "y"
+      ) {
+        let constanant1 = newString.charAt(0);
+        let constanant2 = newString.charAt(1);
+        newString = newString.substring(2);
+        newString = `${newString}${constanant1}${constanant2}ay`;
+      } else {
+        let constanant1 = newString.charAt(0);
+        newString = newString.substring(1);
+        newString = `${newString}${constanant1}ay`;
+      }
+    } else {
+      newString = `${newString}ay`;
+    }
+    if (i === 0) {
+      translated = newString;
+    } else {
+      translated = `${translated} ${newString}`;
+    }
+  }
+  return translated;
 }
 
 module.exports = translate;
--- tddlab1.test.js
+++ tddlab1.test.js
@@ -1,5 +1,63 @@
 const translate = require("./tddlab1");
 
+test("Apple translates to appleay", () => {
+  expect(translate("Apple")).toEqual("appleay");
+});
+
+test("Orange translates to orangeay", () => {
+  expect(translate("Orange")).toEqual("orangeay");
+});
+
+test("Eat translates to eatay", () => {
+  expect(translate("Eat")).toEqual("eatay");
+});
+
+test("Interesting translates to interestingay", () => {
+  expect(translate("Interesting")).toEqual("interestingay");
+});
+
+test("Uber translates to uberay", () => {
+  expect(translate("Uber")).toEqual("uberay");
+});
+
+test("Table translates to abletay", () => {
+  expect(translate("Table")).toEqual("abletay");
+});
+
+test("Mouse translates to ousemay", () => {
+  expect(translate("Mouse")).toEqual("ousemay");
+});
+
+test("Chair translates to airchay", () => {
+  expect(translate("Chair")).toEqual("airchay");
+});
+
+test("Trap translates to aptray", () => {
+  expect(translate("Trap")).toEqual("aptray");
+});
+
+test("Scrap translates to rapscay", () => {
+  expect(translate("Scrap")).toEqual("rapscay");
+});
+
+test("APPLE is converted to lowercase and translates to appleay", () => {
+  expect(translate("APPLE")).toEqual("appleay");
+});
+
+test("I am now fluent in pig latin translates to iay amay ownay uentflay inay igpay atinlay", () => {
+  expect(translate("I am now fluent in pig latin")).toEqual(
+    "iay amay ownay uentflay inay igpay atinlay"
+  );
+});
+
 test("Hello world translates to ellohay orldway", () => {
-  expect(translate("Hello world")).toEqual("Hello world");
+  expect(translate("Hello world")).toEqual("ellohay orldway");
+});
+
+test("Apples and Oranges translates to applesay anday orangesay", () => {
+  expect(translate("Apples and Oranges")).toEqual("applesay anday orangesay");
+});
+
+test("Under the table translates to underay ethay abletay", () => {
+  expect(translate("Under the table")).toEqual("underay ethay abletay");
 });
